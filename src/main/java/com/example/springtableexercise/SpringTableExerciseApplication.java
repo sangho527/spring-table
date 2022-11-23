4e7dbb3b639f40332ba5380fc4c9c7890e835f37
sangho527 <sy980527@naver.com>
2022-11-23 13:46:29 +0900

    first commit

---
+++ src/main/java/com/example/springtableexercise/SpringTableExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtableexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTableExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTableExerciseApplication.class, args);
+	}
+
+}
