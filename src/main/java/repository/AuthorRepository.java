8c112a24bdd41dfb40fb31d1c629e6afcaf721e6
sangho527 <sy980527@naver.com>
2022-11-26 22:50:45 +0900

    [ADD] Author entity, Repository 추가

--- src/main/java/repository/AuthorRepository.java
+++ src/main/java/repository/AuthorRepository.java
@@ -1,4 +1,7 @@
 package repository;
 
-public interface AuthorRepository {
+import domain.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
