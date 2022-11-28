01cdc5500ccdf835eba9d1f71b5e097aaeb2b112
sangho527 <sy980527@naver.com>
2022-11-28 11:29:13 +0900

    [ADD] Review 추가

---
+++ src/main/java/domain/entity/Review.java
@@ -0,0 +1,27 @@
+package domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private String userName;
+
+    @ManyToOne
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+}
